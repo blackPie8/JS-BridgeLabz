920e0acf1b01fb4a898467e7bae4ea9f8226cecc
Ashray Stan <stanashray@gmail.com>
2024-11-28 09:11:24 +0000

    Completed Sequence Practice Problems

---
+++ Practice_1/inches_to_feet.js
@@ -0,0 +1,35 @@
+// a
+
+// function inchestoFeet(inches){
+//     return inches / 12
+// }
+
+// let inches = 42
+// let feet = inchestoFeet(inches)
+// console.log(`${inches} Inches to Feet is ${feet}`);
+
+
+// b
+
+function feetToMeters(feet){
+    return feet*0.3048
+}
+
+let length = 60
+let breadth = 40
+
+let lengthInMeter = feetToMeters(60)
+let breadthInMeter = feetToMeters(40)
+
+// console.log(`${length} x ${breadth} feet is ${lengthInMeter} x ${breadthInMeter} in meters`);
+
+
+// c
+
+let area = (lengthInMeter*breadthInMeter) * 25
+
+function metersToAcres(area){
+    return meters / 4047
+}
+
+console.log(`Area of 25 such plots would be ${area.toFixed(2)} Acres`)
